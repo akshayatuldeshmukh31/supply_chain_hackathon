d1a4f260118af6db6d5cbd2a00b4408d78076597
Akshay Atul Deshmukh <akshayatuldeshmukh31@gmail.com>
2016-11-19 15:01:47 -0800

    Created server startup files

---
+++ api/config/environmentVars.js
@@ -0,0 +1,13 @@
+portNo = process.env.PORT || 3000;
+database = process.env.MONGOLAB_URI || "mongodb://127.0.0.1:27017/hackathon";
+exportInfoCollection = "exportInfo";
+importInfoCollection = "importInfo";
+stakeholdersCollection = "stakeholders";
+drayageRegistryCollection = "drayageRegistry"
+
+exports.portNo = portNo;
+exports.database = database;
+exports.exportInfoCollection = exportInfoCollection;
+exports.importInfoCollection = importInfoCollection;
+exports.stakeholdersCollection = stakeholdersCollection;
+exports.drayageRegistryCollection = drayageRegistryCollection;
