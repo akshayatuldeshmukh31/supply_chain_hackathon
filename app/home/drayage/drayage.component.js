50f41cd16f3789d780e92113f9fae2526eaa6311
Jim Harris <djimharris@gmail.com>
2016-11-20 06:50:13 -0800

    container

--- app/home/drayage/drayage.component.js
+++ app/home/drayage/drayage.component.js
@@ -4,9 +4,24 @@ angular.module('portApp')
 .component('drayage', {
     templateUrl: 'app/home/drayage/drayage.html',
     controller: function($scope, $http) {
-        this.container = undefined;
-        this.searched = true;
-        this.makeAppointment = true;
-        this.detailsWidth = this.makeAppointment ? 'col-xs-6': 'col-xs-12';
+        $scope.container = undefined;
+        $scope.searched = true;
+        $scope.makeAppointment = true;
+        $scope.detailsWidth = $scope.makeAppointment ? 'col-xs-6': 'col-xs-12';
+        $scope.search = function(){
+            $http.post('http://localhost:3000/drayage/checkAvailability',{
+                "container#" : $scope.containerno}).then(function(response) {
+                $scope.details = response.data;
+                if($scope.details.success == 1){
+                    $scope.toggle = true;
+                    $scope.searched = true;
+                }
+                else{
+                    $scope.toggle = false;
+                }
+            }, function(response) {
+                $scope.details = "";
+            });
+        }
     }
 });
