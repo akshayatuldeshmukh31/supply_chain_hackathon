342d92c45121c031927c3ddb403c31173ced0731
Jim Harris <djimharris@gmail.com>
2016-11-20 03:09:52 -0800

    test

--- app/portal/mto/mto.component.js
+++ app/portal/mto/mto.component.js
@@ -9,17 +9,23 @@ angular.module('portApp')
         this.makeAppointment = true;
         this.detailsWidth = this.makeAppointment ? 'col-xs-6': 'col-xs-12';
 
-        $http.post('http://localhost:3000/portadmin/pendingApprovals', {token: authService.getToken()}).then(function(response) {
-            $scope.exports = response.data;
+        $http.post('http://localhost:3000/viewData', {token: authService.getToken()}).then(function(response) {
+            $scope.exports = response.data.exportInfo;
+            $scope.imports = response.data.importInfo;
         }, function(response) {
             console.error('F!');
         });
 
-        $http.post('http://localhost:3000/portadmin/pendingApprovals', {token: authService.getToken()}).then(function(response) {
-            $scope.imports = response.data;
-        }, function(response) {
-            console.error('F!');
-        });
+        $scope.hasArrived = function(){
+         $http.post("http://localhost:3000/portmto/postArrivals", {
+                vesselName: $scope.shipArrived,
+                arrivingTerminal: $scope.shipTerminal,
+            }).then(function(response) {
+                alert('YAY!');
+            }, function(response) {
+                 alert('NAY!');
+            });
+        };
 
     },
     bindings: {
