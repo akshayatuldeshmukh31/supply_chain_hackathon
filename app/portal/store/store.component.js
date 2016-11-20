714d1d910e0164d5340002cae2c11ebb5390c058
Jim Harris <djimharris@gmail.com>
2016-11-20 04:47:23 -0800

    store

--- app/portal/store/store.component.js
+++ app/portal/store/store.component.js
@@ -3,11 +3,20 @@ var angular = require('angular');
 angular.module('portApp')
 .component('store', {
     templateUrl: 'app/portal/store/store.html',
-    controller: function() {
-        this.container = undefined;
-        this.searched = true;
-        this.makeAppointment = true;
-        this.detailsWidth = this.makeAppointment ? 'col-xs-6': 'col-xs-12';
+    controller: function($scope, $http, authService) {
+        $scope.searched = false;
+        $scope.getDetails = function(){
+            $http.post("http://localhost:3000/portmto/postArrivals", {
+                'billOfLading#': $scope.ladingno,
+                token: authService.getToken()
+            }).then(function(response) {
+                $scope.details = response.data;
+                $scope.searched = true;
+            }, function(response) {
+                 alert('NAY!');
+            });
+
+        }
 
 
     },
